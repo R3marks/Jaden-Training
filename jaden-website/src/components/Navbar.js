8ee1c93a08548619cf593cd674425fa410fda6fc
Michael <michael.alexopoulos@bt.com>
2020-12-02 17:02:07 +0000

    02-12-2020

--- jaden-website/src/components/Navbar.js
+++ jaden-website/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import './Navbar.css'
@@ -18,29 +18,33 @@ function Navbar() {
         }
     }
 
+    useEffect(() => {
+        showButton();
+    }, []);
+
     window.addEventListener('resize', showButton);
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-jaden">JADEN</Link>
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>JADEN</Link>
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times': 'fas fa-bars'} />
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className="nav-item">
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            <Link to='/tour' className='nav-links' onClick={closeMobileMenu}>
                                 TOUR
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            <Link to='/merch' className='nav-links' onClick={closeMobileMenu}>
                                 MERCH
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            <Link to='/story' className='nav-links' onClick={closeMobileMenu}>
                                 STORY
                             </Link>
                         </li>
