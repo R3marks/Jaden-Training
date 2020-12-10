eaeefef2342efa2d8d4a8375b761313949a2fcbc
Michael <michael.alexopoulos@bt.com>
2020-12-10 17:47:46 +0000

    10-12-2020

--- jaden-website/src/components/Button.js
+++ jaden-website/src/components/Button.js
@@ -6,14 +6,18 @@ const STYLES = ['btn--cycle', 'btn--navigation'];
 
 const SIZES = ['btn--circle', 'btn--medium', 'btn--large'];
 
-export const Button = ({children, type, onClick, linkTo, buttonStyle, buttonSize}) => {
+const STATES = ['btn--select']
+
+export const Button = ({children, type, onClick, linkTo, buttonStyle, buttonSize, buttonState}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
 
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
+    const checkButtonState = STATES.includes(buttonState) ? buttonState : ''
+
     return (
         <Link to={linkTo} className="btn-mobile">
-            <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
+            <button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonState}`} onClick={onClick} type={type}>
                 {children}
             </button>
         </Link>
