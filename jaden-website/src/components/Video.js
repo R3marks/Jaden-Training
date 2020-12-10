737553e976d08e8a1670b8b33b3e06f40a4806c9
Michael <michael.alexopoulos@bt.com>
2020-12-10 17:47:46 +0000

    10-12-2020

--- jaden-website/src/components/Video.js
+++ jaden-website/src/components/Video.js
@@ -6,9 +6,11 @@ const YOUTUBE_VIDEOS = ["https://www.youtube.com/embed/82UsnbunJUs", "https://ww
 
 function Video() {
     const [video, setVideo] = useState(YOUTUBE_VIDEOS[0]);
+    const [select, setSelect] = useState(null)
 
     function cycleVideo (video) {
         setVideo(YOUTUBE_VIDEOS[video]);
+        setSelect
     };
 
     return (
@@ -18,7 +20,7 @@ function Video() {
                 <div className="video-player">
                     <iframe width="560" height="349" src={video} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="0"></iframe>  
                 </div>
-                <Button buttonStyle="btn--cycle" buttonSize="btn--circle" onClick={() => cycleVideo(0)}></Button>
+                <Button buttonStyle="btn--cycle" buttonSize="btn--circle" buttonState="btn--select" onClick={() => cycleVideo(0)}></Button>
                 <Button buttonStyle="btn--cycle" buttonSize="btn--circle" onClick={() => cycleVideo(1)}></Button>
                 <Button buttonStyle="btn--cycle" buttonSize="btn--circle" onClick={() => cycleVideo(2)}></Button>
                 <Button buttonStyle="btn--cycle" buttonSize="btn--circle" onClick={() => cycleVideo(3)}></Button>
