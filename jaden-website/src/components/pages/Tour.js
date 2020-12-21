bca112dc23a1e2bf9f183ec9c66a70188b294e3c
Michael <r3marks@hotmail.com>
2020-12-21 18:58:25 +0000

    21-12-2020

---
+++ jaden-website/src/components/pages/Tour.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../App.css';
+import Navbar from '../Navbar'
+import Tours from '../Tours'
+
+function Tour() {
+    return (
+        <>
+            <Navbar background="#223B6E" opacity="0.9" />
+            <Tours />
+        </>
+    )
+}
+
+export default Tour
