8ad35bffcce74ccfa80c5948d5d8fbfea30c3dd1
Michael <r3marks@hotmail.com>
2020-12-22 22:51:39 +0000

    22-12-2020 tour page in react complete

--- jaden-website/src/components/Tours.js
+++ jaden-website/src/components/Tours.js
@@ -14,8 +14,10 @@ function Tours() {
                         </input>
                     </div>
                     <div className="scroll-box-tickets">
-                        <TourItem />
-                        <TourItem />
+                        <TourItem date="MAY 26" city="SAN DIEGO, CA" arena="PECHANGA ARENA" link="https://pechangaarenasd.com/" />
+                        <TourItem date="JUN 19" city="MINNEAPOLIS, MN" arena="TARGET CENTER" link="https://www.targetcenter.com/" />
+                        <TourItem date="JUN 28" city="DETROIT, MN" arena="LITTLE CAESARS ARENA" link="https://www.313presents.com/" />
+                        <TourItem date="JUN 29" city="COLOMBUS, OH" arena="THE SCHOTTENSTEIN CENTER" link="https://www.schottensteincenter.com/" />
                     </div>
                 </div>
             </div>
