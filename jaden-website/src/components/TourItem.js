bc27408e3f8458292550d83ac8a8d114689ca2a2
Michael <r3marks@hotmail.com>
2020-12-22 22:51:39 +0000

    22-12-2020 tour page in react complete

--- jaden-website/src/components/TourItem.js
+++ jaden-website/src/components/TourItem.js
@@ -6,11 +6,14 @@ function TourItem(props) {
     return (
         <div className="tour-item">
             <div className="tour-info">
-                <strong className="tour-date">MAY 26</strong>
-                <span className="tour-city">SAN DIEGO, CA</span>
-                <span className="tour-arena"> PECHANGA ARENA</span>  
+                <strong className="tour-date">{props.date}</strong>
+                <span className="tour-city">{props.city}</span>
+                <a href={props.link} target="_blank" rel="noreferrer" className="map-location">
+                    <i class="fas fa-map-marked-alt"></i>
+                </a>
+                <span className="tour-arena">{props.arena}</span>  
             </div>
-            <Button buttonStyle="btn--buy" buttonSize="btn--medium" >BUY TICKETS</Button>
+            <Button buttonStyle="btn--buy" buttonSize="btn--medium">BUY TICKETS</Button>
         </div>
     )
 }
