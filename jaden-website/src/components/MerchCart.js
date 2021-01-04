c47905e457f20adde127f5063a0ab12e48434661
Michael <michael.alexopoulos@bt.com>
2021-01-04 17:29:17 +0000

    04-01-2021

--- jaden-website/src/components/MerchCart.js
+++ jaden-website/src/components/MerchCart.js
@@ -12,21 +12,83 @@ function MerchCart() {
         "syre.jpg"
     ]
 
-    const [cart, setCart] = useState([])
+    const MERCH = [{
+        src: "twitter-icon.png",
+        name: "TWITTER PIN",
+        price: "£0.99"
+    }, {
+        src: "ctv3_tshirt.png",
+        name: "CTV3 T-SHIRT",
+        price: "£19.99"
+    }]
 
-    function test(element) {
+    const [cart, setCart] = useState([])
+    const [total, setTotal] = useState(0)
+
+    const removePoundSign = new RegExp('(?<=£).*')
+
+    function updateTotal(price) {
+        console.log(price)
+        let numericPrice = price.match(removePoundSign)
+        var roundedTotal = +((total + Number(numericPrice[0])).toFixed(2))
+        setTotal(roundedTotal)
+        console.log(total)
+    }
+
+    function addProductToCart(element) {
         let parent = element.target.parentElement
         var merchInfo = parent.previousElementSibling
         var merchImage = merchInfo.previousElementSibling
         var regex = new RegExp('(?<=Jaden/).*')
         var merchSrc = merchImage.src.match(regex)
         var [productName, productPrice] = merchInfo.children
-        console.log(merchSrc[0])
-        // CART.push(merchSrc[0])
+        var cartProduct = {
+            src: merchSrc[0],
+            name: productName.innerText,
+            price: productPrice.innerText
+        }
+        console.log(cartProduct)
         console.log(cart)
-        const newCart = [...cart, merchSrc[0]]
-        console.log(newCart)
+        for (let i = 0; i < cart.length; i++) {
+            console.log(cart[i])
+            console.log(cartProduct)
+            if (JSON.stringify(cart[i]) === JSON.stringify(cartProduct)) {
+                console.log('HERE')
+                alert('Item has already been added!')
+                return
+            }  
+        }
+        const newCart = [...cart, cartProduct]
         setCart(newCart)
+        updateTotal(productPrice.innerText)
+    }
+
+    function removeProductFromCart(element) {
+        let parent = element.target.parentElement
+        var cartQuantityInfo = parent.parentElement
+        var cartProductPrice = cartQuantityInfo.nextElementSibling
+        console.log(cartProductPrice)
+        var cartProductInfo = cartQuantityInfo.previousElementSibling.previousElementSibling
+        var [productImage, productName] = cartProductInfo.children
+        var regex = new RegExp('(?<=Jaden/).*')
+        var merchSrc = productImage.src.match(regex)
+        console.log(merchSrc[0])
+        var productToRemove = {
+            src: merchSrc[0],
+            name: productName.innerText,
+            price: cartProductPrice.innerText
+        }
+        const newCart = []
+        for (let i = 0; i < cart.length; i++) {
+            if (JSON.stringify(cart[i]) !== JSON.stringify(productToRemove)) {
+                newCart.push(cart[i])
+            }
+        }
+        console.log(cart)
+        setCart(newCart)
+        console.log(newCart)
+        var negativePrice = cartProductPrice.innerText.slice(0, 1) + "-" + cartProductPrice.innerText.slice(1)
+        updateTotal(negativePrice)
     }
 
     const [quantity, setQuantity] = useState(1)
@@ -39,7 +101,6 @@ function MerchCart() {
         } else {
             setQuantity(event.target.value)  
         }
-
     }
 
     let refContainers = useRef([])
@@ -64,14 +125,14 @@ function MerchCart() {
             <h1 className="merch-header">MERCH</h1>
                 <div className="merch-section">
                     <div className="scroll-box-merch">
-                        {IMAGES.map((image, index) => (
+                        {MERCH.map((merch, index) => (
                             <div className="merch-product" key={index}>
-                                <img className="merch-image" src={"./Images - Jaden/" + image}></img>
+                                <img className="merch-image" src={"./Images - Jaden/" + merch.src}></img>
                                 <div className="merch-info">
-                                    <span>CTV3 T-SHIRT</span>
-                                    <span>£19.99</span>
+                                    <span>{merch.name}</span>
+                                    <span>{merch.price}</span>
                                 </div>
-                                <Button buttonStyle="btn--buy" buttonSize="btn--medium"onClick={test.bind(this)}>ADD TO CART</Button>
+                                <Button buttonStyle="btn--buy" buttonSize="btn--medium" onClick={addProductToCart.bind(this)}>ADD TO CART</Button>
                             </div> 
                         ))}
                     </div>
@@ -89,12 +150,12 @@ function MerchCart() {
                         <span className="cart-header-price">PRICE</span>
                     </div>
                     <div className="scroll-box-cart">
-                        {cart.map((image, index) => (
+                        {cart.map((product, index) => (
                             <div className="cart-products"
                             key={index}>
                             <div className="cart-product">
-                                <img className="cart-image" src={"./Images - Jaden/" + image}></img>
-                                <span className="cart-name">CTV3 T-SHIRT</span>
+                                <img className="cart-image" src={"./Images - Jaden/" + product.src}></img>
+                                <span className="cart-name">{product.name}</span>
                             </div>
                             <div className="cart-size">
                                 <Button ref={(Button) => refContainers.current[0] = Button} buttonStyle="btn--size" buttonSize="btn--square" onClick={() => selectSize(0)}>S</Button>
@@ -103,16 +164,16 @@ function MerchCart() {
                             </div>
                             <div className="cart-quantity">
                                 <input type="number" value={quantity} onChange={e => changeQuantity(e)}></input>
-                                <Button buttonStyle="btn--buy" buttonSize="btn--medium">REMOVE</Button>
+                                <Button buttonStyle="btn--buy" buttonSize="btn--medium" onClick={removeProductFromCart.bind(this)}>REMOVE</Button>
                             </div>
-                            <span className="cart-price">£19.99</span>
+                            <span className="cart-price">{product.price}</span>
                         </div>
                         ))}
                         {/* <CartProduct src="ctv3_hoodie.png" />   */}
                     </div>
                     <div className="total-row">
                         <span className="total-name">Total</span>
-                        <span className="total-price">£39.98</span>
+                        <span className="total-price">£{total}</span>
                         <Button buttonStyle="btn--buy" buttonSize="btn--medium">PURCHASE</Button>
                     </div>
                 </div>
