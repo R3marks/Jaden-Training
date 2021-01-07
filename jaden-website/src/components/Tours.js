5edbf1382fbe9a0aab2899ddca8065a2f34cb605
Michael <michael.alexopoulos@bt.com>
2021-01-07 17:35:01 +0000

    07-01-2021

--- jaden-website/src/components/Tours.js
+++ jaden-website/src/components/Tours.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import './Tours.css'
-import TourItem from './TourItem'
+import { Button } from './Button'
 
 function Tours() {
+
+    const TOUR = [{
+        date: "MAY 26",
+        city: "SAN DIEGO, CA",
+        link: "https://goo.gl/maps/jDKwrSR9XokpJpEr6",
+        arena: "PECHANGA ARENA"
+    }, {
+        date: "JUN 19",
+        city: "MINNEAPOLIS, MN",
+        link: "https://g.page/TargetCenterMN?share",
+        arena: "TARGET CENTER"
+    }, {
+        date: "JUN 28",
+        city: "DETROIT, MN",
+        link: "https://goo.gl/maps/9TE2az6pbm65J3jCA",
+        arena: "LITTLE CAESARS ARENA"
+    }, {
+        date: "JUN 29",
+        city: "COLOMBUS, OH",
+        link: "https://goo.gl/maps/YSGcvvHXZJBYycZN7",
+        arena: "THE SCHOTTENSTEIN CENTER"
+    }]
+
+    function buyTickets() {
+        alert('Tickets purchased')
+    }
+
     return (
         <div className="tour-background">
             <div className="tour-wrapper">
@@ -14,10 +41,19 @@ function Tours() {
                         </input>
                     </div>
                     <div className="scroll-box-tickets">
-                        <TourItem date="MAY 26" city="SAN DIEGO, CA" arena="PECHANGA ARENA" link="https://pechangaarenasd.com/" />
-                        <TourItem date="JUN 19" city="MINNEAPOLIS, MN" arena="TARGET CENTER" link="https://www.targetcenter.com/" />
-                        <TourItem date="JUN 28" city="DETROIT, MN" arena="LITTLE CAESARS ARENA" link="https://www.313presents.com/" />
-                        <TourItem date="JUN 29" city="COLOMBUS, OH" arena="THE SCHOTTENSTEIN CENTER" link="https://www.schottensteincenter.com/" />
+                        {TOUR.map((tour, index) => (
+                            <div className="tour-item" key={index}>
+                                <div className="tour-info">
+                                    <strong className="tour-date">{tour.date}</strong>
+                                    <span className="tour-city">{tour.city}</span>
+                                    <a href={tour.link} target="_blank" rel="noreferrer" className="map-location">
+                                        <i class="fas fa-map-marked-alt"></i>
+                                    </a>
+                                    <span className="tour-arena">{tour.arena}</span>  
+                                </div>
+                                <Button buttonStyle="btn--buy"      buttonSize="btn--medium" onClick={buyTickets}>BUY TICKETS</Button>
+                            </div> 
+                        ))}
                     </div>
                 </div>
             </div>
