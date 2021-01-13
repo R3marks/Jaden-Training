e7946c74473969de95da6750f74f8b1d79f58ec6
Michael <michael.alexopoulos@bt.com>
2021-01-13 17:36:33 +0000

    13-01-2020

--- jaden-backend/resolvers.js
+++ jaden-backend/resolvers.js
@@ -2,9 +2,25 @@ const db = require('./db')
 
 const Query = {
     tour: (parent, args) => db.tours.get(args.id),
-    tours: () => db.tours.list()
+    tours: () => db.tours.list(),
+    searchTour: (parent, args) => searchTours(parent, args)// (parent, args) => db.tours.list().length
 }
 
-console.log(db.tours.get({id: "1"})) //? (Does work tho)
+function searchTours(parent, args) {
+    console.log(parent)
+    console.log(args)
+    let results = new Set
+
+    db.tours.list().forEach(tour => {
+        for (key in tour) {
+            if (tour[key].indexOf(args.search) != -1 && key !== "link") {
+                results.add(tour)
+            }
+        }
+    })
+    console.log(results)
+    return results
+}
+
 
 module.exports = { Query }
