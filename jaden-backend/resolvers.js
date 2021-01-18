4dff0835dc9599bec0b26c673d3e3038a859f5c3
Michael <michael.alexopoulos@bt.com>
2021-01-18 17:30:27 +0000

    18-01-2021 Completed graphql-tour

--- jaden-backend/resolvers.js
+++ jaden-backend/resolvers.js
@@ -1,25 +1,51 @@
+const { ApolloError } = require('apollo-server')
 const db = require('./db')
 
 const Query = {
-    tour: (parent, args) => db.tours.get(args.id),
-    tours: () => db.tours.list(),
-    searchTour: (parent, args) => searchTours(parent, args)// (parent, args) => db.tours.list().length
+    tour: (parent, args) => getTourByID(parent, args),
+    tours: () => getAllTours(),
+    searchTour: (parent, args) => searchTours(parent, args)
+}
+
+function getTourByID(parent, args) {
+    try {
+    var result = db.tours.get(args.id)
+    if (!result) {
+        return new ApolloError(`Could not find Tour with ID: ${args.id}`, "DATABASE_ENTRY_NOT_FOUND")
+    }
+    return result
+    } catch (error) {
+        return new ApolloError('Could not get tour by ID in database', 'DATABASE_ERROR')
+    }
+}
+
+function getAllTours() {
+    try {
+        var result = db.tours.list()
+        if (!result) {
+            return new ApolloError('Could not find all Tours', 'DATABASE_TABLE_NOT_FOUND')
+        }
+        return result
+    } catch (error) {
+        return new ApolloError('Could not get all tours from database', 'DATABASE_ERROR')
+    }
 }
 
 function searchTours(parent, args) {
-    console.log(parent)
-    console.log(args)
-    let results = new Set
+    try {
+        let results = new Set
 
-    db.tours.list().forEach(tour => {
-        for (key in tour) {
-            if (tour[key].indexOf(args.search) != -1 && key !== "link") {
-                results.add(tour)
+        db.tours.list().forEach(tour => {
+            for (key in tour) {
+                if (tour[key].indexOf(args.search) != -1 && key !== "link") {
+                    results.add(tour)
+                }
             }
-        }
-    })
-    console.log(results)
-    return results
+        })
+        return results
+    } catch (error) {
+        return new ApolloError('Could not search through database for tours', 'DATABASE_ERROR')
+    }
 }
 
 
