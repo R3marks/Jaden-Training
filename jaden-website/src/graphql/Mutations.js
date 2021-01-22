ea40952ae46bfc718af6a28bebb01ea5a45d2cae
Michael <r3marks@hotmail.com>
2021-01-22 23:31:15 +0000

    22-01-2021

--- jaden-website/src/graphql/Mutations.js
+++ jaden-website/src/graphql/Mutations.js
@@ -1,13 +1,27 @@
 import { gql } from '@apollo/client'
 
 export const ADD_TO_CART = gql`
-    mutation AddToCart($idProvided: ID!) {
+    mutation addToCart($idProvided: ID!) {
         addToCart(id: $idProvided) {
             cart {
                 id
                 src 
                 name 
                 price 
+                quantity
+            }
+        }
+    }
+`
+
+export const REMOVE_FROM_CART = gql`
+    mutation deleteCartItemById($idProvided: ID!) {
+        deleteCartItemById(id: $idProvided) {
+            cart {
+                id
+                src
+                name
+                price
                 quantity
             }
         }
