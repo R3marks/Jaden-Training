d8e12980cf92fb2450aeed66a265b7ac1512941a
Michael <r3marks@hotmail.com>
2021-01-29 22:55:01 +0000

    29-01-2021

--- jaden-website/src/components/MerchCart.js
+++ jaden-website/src/components/MerchCart.js
@@ -2,37 +2,8 @@ import React from 'react'
 import './MerchCart.css'
 import MerchQuery from './MerchQuery'
 import CartQuery from './CartQuery'
-import { onError } from "@apollo/client/link/error";
 
 function MerchCart() {
-
-    let x = 3
-
-    function checkMerchQuery() {
-        if (x == 4) {
-            return <h1 className="empty-cart">Server Offline</h1>
-        } else {
-            return <MerchQuery />
-        }
-    }
-
-    function checkCartQuery() {
-        if (x == 4) {
-            return <h1 className="empty-cart">Server Offline</h1>
-        } else {
-            return <CartQuery />
-        }
-    }
-
-    onError(({ graphQLErrors, networkError }) => {
-        if (graphQLErrors)
-            graphQLErrors.map(({ message, locations, path }) =>
-            console.log(
-                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-            ),
-        );
-        if (networkError) console.log(`[Network error]: ${networkError}`);
-    });
 
     // save scroll position
 
@@ -43,7 +14,7 @@ function MerchCart() {
             <h1 className="merch-header">MERCH</h1>
                 <div className="merch-section">
                     <div className="scroll-box-merch">
-                        {checkMerchQuery()}
+                        <MerchQuery />
                     </div>
                 </div>
             </div>
@@ -58,7 +29,7 @@ function MerchCart() {
                         <span>QUANTITY</span>
                         <span className="cart-header-price">PRICE</span>
                     </div>
-                        {checkCartQuery()}
+                    <CartQuery />
                 </div>
             </div>
         </div>
