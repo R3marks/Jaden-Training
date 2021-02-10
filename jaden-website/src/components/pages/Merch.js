560f9ab1fdb355a883113356d83ed7c6a70c2f4c
Michael <michael.alexopoulos@bt.com>
2021-02-10 17:35:23 +0000

    10-02-2021

--- jaden-website/src/components/pages/Merch.js
+++ jaden-website/src/components/pages/Merch.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../../App.css';
 import Navbar from '../Navbar'
 import MerchSection from '../MerchSection'
 import CartSection from '../CartSection'
+import { AuthContext } from '../AuthProvider';
 
 export default function Merch () {
+
+    const { isAuthenticated } = useContext(AuthContext)
+    var result = isAuthenticated()
+
     return (
         <>
             <Navbar />
             <MerchSection />
-            <CartSection />
+            {result ? <CartSection /> : undefined}
         </>
     )
 }
