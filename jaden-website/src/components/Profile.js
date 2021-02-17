d3b3adceda3f580c7f04922066acb507882920de
Michael <michael.alexopoulos@bt.com>
2021-02-17 17:01:48 +0000

    All necessary changes incorporated for a user to have their own cart

--- jaden-website/src/components/Profile.js
+++ jaden-website/src/components/Profile.js
@@ -1,27 +1,30 @@
 import React, { useContext } from 'react'
 import { AuthContext } from './AuthProvider'
 import ActionButton from './ActionButton'
-import { useMutation } from '@apollo/client'
+import { useApolloClient, useMutation } from '@apollo/client'
 import { SIGN_OUT } from '../graphql/Mutations'
 
 function Profile() {
 
     const { setAuthInfo } = useContext(AuthContext)
+
+    const client = useApolloClient()
     const [signOutUser, { loading, error }] = useMutation(SIGN_OUT)
 
     async function handleSignOut() {
         await signOutUser()
+        await client.resetStore()
         setAuthInfo({ userData: null })
     }
 
     return (
         <>
-        <div className="auth-headers">
-            <h1 className='auth-header' >PROFILE</h1>
-        </div>
-        <div className="auth-section">
-            <ActionButton buttonSize='btn--large' buttonStyle='btn--buy' onClick={handleSignOut}>Sign out</ActionButton>
-        </div>
+            <div className="auth-headers">
+                <h1 className='auth-header'>PROFILE</h1>
+            </div>
+            <div className="auth-section">
+                <ActionButton buttonSize='btn--large' buttonStyle='btn--buy' onClick={handleSignOut}>Sign out</ActionButton>
+            </div>
         </>
     )
 }
