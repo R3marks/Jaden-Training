3a16da5a8f579ef141c23c384351722332c4e376
Michael <michael.alexopoulos@bt.com>
2021-02-23 18:08:11 +0000

    23-02-2021

--- jaden-website/src/components/CartQuery.js
+++ jaden-website/src/components/CartQuery.js
@@ -4,19 +4,18 @@ import { useQuery, useMutation } from '@apollo/client'
 import { GET_CART } from '../graphql/Queries'
 import { PURCHASE_CART, REMOVE_FROM_CART, UPDATE_QUANTITY } from '../graphql/Mutations'
 import ActionButton from './ActionButton'
+import UnknownError from './UnknownError'
 
 function CartQuery() {
 
-    const [errorMessage, setErrorMessage] = useState(null)
+    const [unknownError, setUnknownError] = useState(null)
     const [sizeArray, setSizeArray] = useState(['', 'btn--select', ''])
     const scrollBoxCart = useRef(null)
 
     const { loading, error, data } = useQuery(GET_CART, 
         { onError: (errors) => {
-            if (errors.message === 'Failed to fetch') {
-                setErrorMessage('Server Offline')
-            } else {
-                console.log(JSON.stringify(errors))
+            if (errors.message !== 'Failed to fetch') {
+                setUnknownError(errors)
             }
         }
     })
@@ -89,7 +88,7 @@ function CartQuery() {
                 idProvided: cartId
             }})
         } catch (errors) {
-            console.log(JSON.stringify(errors))
+            setUnknownError(errors)
         }
         scrollBoxCart.current.scrollTop = prevScrollTop
     }
@@ -108,7 +107,7 @@ function CartQuery() {
                 idProvided: id, newQuantity: parseInt(input.value)
             }})
         } catch (errors) {
-            console.log(JSON.stringify(errors))
+            setUnknownError(errors)
         }
     }
 
@@ -117,15 +116,17 @@ function CartQuery() {
             purchaseCart()
             alert("Purchase Completed")
         } catch (errors) {
-            console.log(JSON.stringify(errors))
+            setUnknownError(errors)
         }
     }
-
+    console.log(data)
     // Queries need to be handled better. It doesnt make sense to re-render everytime you change the quantity
     if (loading) return <h1 className="empty-cart">Loading...</h1>;
-    if (error) return <h1 className="empty-cart">{errorMessage}</h1>
-    if (data.allCart && data.allCart.cartItems.length === 0) return <h1 className="empty-cart">Your cart is empty</h1>
-    if (!data.allCart) return <h1 className="empty-cart">Your cart is empty</h1>
+    if (unknownError) return <UnknownError errors={unknownError} />
+    if (error && !unknownError) return <h1 className="empty-cart">Server Offline {JSON.stringify(error)}</h1>
+    // if (data.allCart && data.allCart.cartItems.length === 0) return <h1 className="empty-cart">Your cart is empty</h1>
+    if (!data.allCart || data?.allCart?.cartItems?.length === 0) return <h1 className="empty-cart">Your cart is empty</h1>
+
 
     return (
         <>
@@ -144,7 +145,7 @@ function CartQuery() {
                     </div>
                     <div className="cart-quantity">
                         <input type="number" value={product.quantity} onChange={changeQuantity} disabled={loadUpdateQuantity}></input>
-                        <ActionButton buttonStyle="btn--buy" buttonSize="btn--medium" onClick={removeProductFromCart} disabled={loadRemoveFromCart}>REMOVE</ActionButton>
+                        <ActionButton dataTestId={`removeFromCart-${product.id}`} buttonStyle="btn--buy" buttonSize="btn--medium" onClick={removeProductFromCart} disabled={loadRemoveFromCart}>REMOVE</ActionButton>
                     </div>
                     <span className="cart-price">£{(product.price * product.quantity).toFixed(2)}</span>
                 </div>
